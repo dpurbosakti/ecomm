72bf8e5f322369e358503910dfd1f05f67f43a53
dpurbosakti <dwiatmokop@gmail.com>
2024-06-27 17:17:07 +0700

    feat(product) : add create and get

---
+++ ecomm-api/storer/storer.go
@@ -0,0 +1,43 @@
+package storer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type MySQLStorer struct {
+	db *sqlx.DB
+}
+
+func NewMYSQLStorer(db *sqlx.DB) *MySQLStorer {
+	return &MySQLStorer{
+		db: db,
+	}
+}
+
+func (ms *MySQLStorer) CreateProduct(ctx context.Context, p *Product) (*Product, error) {
+	res, err := ms.db.NamedExecContext(ctx, "INSERT INTO products (name, image, category, description, rating, num_reviews, price, count_in_stock) VALUES (:name, :image, :category, :description, :rating, :num_reviews, :price, :count_in_stock)", p)
+	if err != nil {
+		return nil, fmt.Errorf("error inserting product: %w", err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, fmt.Errorf("error getting last insert ID: %w", err)
+	}
+	p.ID = id
+
+	return p, nil
+}
+
+func (ms *MySQLStorer) GetProduct(ctx context.Context, id int64) (*Product, error) {
+	var p Product
+	err := ms.db.GetContext(ctx, &p, "SELECT * FROM products WHERE id=?", id)
+	if err != nil {
+		return nil, fmt.Errorf("error getting product: %w", err)
+	}
+
+	return &p, nil
+}
